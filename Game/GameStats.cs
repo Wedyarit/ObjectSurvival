c872ee21a9e7c7b907684b0642b6562e50cb5c48
Wedyarit <48180322+Wedyarit@users.noreply.github.com>
2021-01-24 22:02:44 +0600

    Update GameStats.cs

--- Game/GameStats.cs
+++ Game/GameStats.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,7 +23,7 @@ namespace Game
             BonusDropChance = bonusDropChance;
             ObstacleDropChance = obstacleDropChance;
             LivesCount = livesCount;
-            LastEvent = new GameEvent("Игра началась");
+            LastEvent = new GameEvent("Game Started");
             StartedAt = DateTime.Now;
             Record = new Record();
             Record.ReadRecord();
